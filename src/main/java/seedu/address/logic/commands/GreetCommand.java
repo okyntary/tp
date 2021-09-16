46587be9bca3395294927c922686d0a1c10aa964
Jovyn Tan <jovyn.tls@gmail.com>
2021-09-16 21:04:43 +0800

    Add Greet command

---
+++ src/main/java/seedu/address/logic/commands/GreetCommand.java
@@ -0,0 +1,23 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import seedu.address.model.Model;
+
+/**
+ * Lists all persons in the address book to the user.
+ */
+public class GreetCommand extends Command {
+
+    public static final String COMMAND_WORD = "greet";
+
+    public static final String MESSAGE_SUCCESS = "Hello! I am ePoch!";
+
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
