44ce8d33d68642b910738c86234dfa66f72449b4
okyntary <okyntary@gmail.com>
2021-10-08 12:46:24 +0800

    Fix more checkstyle violations

--- src/test/java/seedu/address/storage/JsonAdaptedCcaTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedCcaTest.java
@@ -1,9 +1,9 @@
 package seedu.address.storage;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.testutil.TypicalCcas.NUSSO;
+
+import org.junit.jupiter.api.Test;
 
 class JsonAdaptedCcaTest {
 
@@ -14,16 +14,5 @@ class JsonAdaptedCcaTest {
         JsonAdaptedCca cca = new JsonAdaptedCca(NUSSO);
         assertEquals(NUSSO, cca.toModelType());
     }
-
-//    Problem - JsonAdaptedCca(null) is ambiguous constructor and Java can't tell which constructor to call
-//    Not a problem for JsonAdaptedPerson because other arguments are passed in as well and Java can tell which
-//    constructor is called
-
-//    @Test
-//    public void toModelType_nullName_throwsIllegalValueException() {
-//        JsonAdaptedCca cca = new JsonAdaptedCca(null);
-//        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Cca.class.getSimpleName());
-//        assertThrows(IllegalValueException.class, expectedMessage, cca::toModelType);
-//    }
 
 }
