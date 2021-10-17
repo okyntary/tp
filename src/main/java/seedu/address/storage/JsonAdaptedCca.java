cc53da835066c7d3620c937f6faa707b4c7f3025
okyntary <okyntary@gmail.com>
2021-10-17 22:48:35 +0800

    Fix checkstyle

--- src/main/java/seedu/address/storage/JsonAdaptedCca.java
+++ src/main/java/seedu/address/storage/JsonAdaptedCca.java
@@ -1,5 +1,11 @@
 package seedu.address.storage;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -7,12 +13,6 @@ import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.cca.Cca;
 import seedu.address.model.cca.CcaName;
 import seedu.address.model.person.Person;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * Jackson-friendly version of {@link Cca}.
@@ -27,7 +27,8 @@ public class JsonAdaptedCca {
      * Constructs a {@code JsonAdaptedCca} with the given cca details.
      */
     @JsonCreator
-    public JsonAdaptedCca(@JsonProperty("name") String name, @JsonProperty("members")Set<JsonAdaptedPerson> personArrayList) {
+    public JsonAdaptedCca(@JsonProperty("name") String name,
+                          @JsonProperty("members")Set<JsonAdaptedPerson> personArrayList) {
         this.name = name;
         if (personArrayList != null) {
             this.personArrayList.addAll(personArrayList);
