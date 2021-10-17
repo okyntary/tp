3aa45e85ad202b261f857bf0c6776f1b4ec859f4
Jovyn Tan <jovyn.tls@gmail.com>
2021-10-18 00:57:43 +0800

    Implement delete command for reminders

---
+++ src/main/java/seedu/address/logic/parser/reminder/ReminderDeleteCommandParser.java
@@ -0,0 +1,29 @@
+package seedu.address.logic.parser.reminder;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.reminder.ReminderDeleteCommand;
+import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.ParserUtil;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new ReminderDeleteCommand object
+ */
+public class ReminderDeleteCommandParser implements Parser<ReminderDeleteCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the PersonDeleteCommand
+     * and returns a PersonDeleteCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public ReminderDeleteCommand parse(String args) throws ParseException {
+        try {
+            Index index = ParserUtil.parseIndex(args);
+            return new ReminderDeleteCommand(index);
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ReminderDeleteCommand.MESSAGE_USAGE), pe);
+        }
+    }
+}
