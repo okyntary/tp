c12d80e80b6714d426d73c0dde9d32379f58b798
jivesh <jiveshm2000@gmail.com>
2021-10-25 20:25:24 +0800

    Show associated CCAs and reminders when finding for person

--- src/main/java/seedu/address/logic/commands/person/PersonFindCommand.java
+++ src/main/java/seedu/address/logic/commands/person/PersonFindCommand.java
@@ -2,11 +2,24 @@ package seedu.address.logic.commands.person;
 
 import static java.util.Objects.requireNonNull;
 
+import javafx.collections.ObservableList;
 import seedu.address.commons.core.Messages;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.model.Model;
+import seedu.address.model.cca.Cca;
+import seedu.address.model.cca.CcaName;
+import seedu.address.model.cca.CcaNameContainsKeywordsPredicate;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.Person;
+import seedu.address.model.reminder.Reminder;
+import seedu.address.model.reminder.ReminderName;
+import seedu.address.model.reminder.ReminderNameContainsKeywordsPredicate;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Finds and lists all persons in address book whose name contains any of the argument keywords.
@@ -14,7 +27,7 @@ import seedu.address.model.person.NameContainsKeywordsPredicate;
  */
 public class PersonFindCommand extends Command {
 
-    public static final String COMMAND_WORD = "find";
+    public static final String COMMAND_WORD = "findp";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
             + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
@@ -31,8 +44,39 @@ public class PersonFindCommand extends Command {
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredPersonList(predicate);
+
+        CcaNameContainsKeywordsPredicate ccaPredicate = getFilterForCcaList(model);
+        model.updateFilteredCcaList(ccaPredicate);
+
+        ReminderNameContainsKeywordsPredicate reminderPredicate = getFilterForRemainderList(model.getFilteredCcaList());
+        model.updateFilteredReminderList(reminderPredicate);
+
         return new CommandResult(
                 String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+    }
+
+    private CcaNameContainsKeywordsPredicate getFilterForCcaList(Model model) {
+        Set<Cca> allValidCcas = new HashSet<>();
+        for (Person validPerson: model.getFilteredPersonList()) {
+            for (Cca cca: model.getFilteredCcaList()) {
+                if (cca.checkPerson(validPerson)) {
+                    allValidCcas.add(cca);
+                }
+            }
+        }
+        List<String> allCcaNames = allValidCcas
+                .stream().map(Cca::getName).map(CcaName::toString).collect(Collectors.toList());
+        return new CcaNameContainsKeywordsPredicate(allCcaNames);
+    }
+
+    private ReminderNameContainsKeywordsPredicate getFilterForRemainderList(ObservableList<Cca> ccaList) {
+        Set<Reminder> allReminders = new HashSet<>();
+        for (Cca validCca: ccaList) {
+            allReminders.addAll(validCca.getReminders());
+        }
+        List<String> allReminderNames = allReminders
+                .stream().map(Reminder::getName).map(ReminderName::toString).collect(Collectors.toList());
+        return new ReminderNameContainsKeywordsPredicate(allReminderNames);
     }
 
     @Override
