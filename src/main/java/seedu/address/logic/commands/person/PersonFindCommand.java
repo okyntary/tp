04510df68bf40311d87bd670ae545bae390243de
jivesh <jiveshm2000@gmail.com>
2021-11-01 21:59:33 +0800

    Add separate predicates for findp to find associated CCAs

--- src/main/java/seedu/address/logic/commands/person/PersonFindCommand.java
+++ src/main/java/seedu/address/logic/commands/person/PersonFindCommand.java
@@ -13,8 +13,7 @@ import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.model.Model;
 import seedu.address.model.cca.Cca;
-import seedu.address.model.cca.CcaName;
-import seedu.address.model.cca.CcaNameContainsKeywordsPredicate;
+import seedu.address.model.cca.CcaContainsPersonPredicate;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
 import seedu.address.model.reminder.Reminder;
@@ -47,38 +46,15 @@ public class PersonFindCommand extends Command {
 
         model.updateFilteredPersonList(predicate);
 
-        CcaNameContainsKeywordsPredicate ccaPredicate = getFilterForCcaList(model);
+        CcaContainsPersonPredicate ccaPredicate = getFilterForCcaList(model.getFilteredPersonList());
         model.updateFilteredCcaList(ccaPredicate);
-
-        ReminderNameContainsKeywordsPredicate reminderPredicate = getFilterForRemainderList(model.getFilteredCcaList());
-        model.updateFilteredReminderList(reminderPredicate);
 
         return new CommandResult(
                 String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
     }
 
-    private CcaNameContainsKeywordsPredicate getFilterForCcaList(Model model) {
-        Set<Cca> allValidCcas = new HashSet<>();
-        for (Person validPerson: model.getFilteredPersonList()) {
-            for (Cca cca: model.getFilteredCcaList()) {
-                if (cca.containsEnrolledPerson(validPerson)) {
-                    allValidCcas.add(cca);
-                }
-            }
-        }
-        List<String> allCcaNames = allValidCcas
-                .stream().map(Cca::getName).map(CcaName::toString).collect(Collectors.toList());
-        return new CcaNameContainsKeywordsPredicate(allCcaNames);
-    }
-
-    private ReminderNameContainsKeywordsPredicate getFilterForRemainderList(ObservableList<Cca> ccaList) {
-        Set<Reminder> allReminders = new HashSet<>();
-        for (Cca validCca: ccaList) {
-            allReminders.addAll(validCca.getReminders());
-        }
-        List<String> allReminderNames = allReminders
-                .stream().map(Reminder::getName).map(ReminderName::toString).collect(Collectors.toList());
-        return new ReminderNameContainsKeywordsPredicate(allReminderNames);
+    private CcaContainsPersonPredicate getFilterForCcaList(List<Person> personList) {
+        return new CcaContainsPersonPredicate(personList);
     }
 
     @Override
---
+++ src/main/java/seedu/address/model/cca/CcaContainsPersonPredicate.java
@@ -0,0 +1,27 @@
+package seedu.address.model.cca;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.model.person.Person;
+
+/**
+ * Tests that a {@code Person}'s {@code CcaName} matches any of the keywords given.
+ * To be used by findCcaCommand, but not implemented yet
+ */
+public class CcaContainsPersonPredicate implements Predicate<Cca> {
+    private final List<Person> persons;
+
+    public CcaContainsPersonPredicate(List<Person> persons) {
+        this.persons = persons;
+    }
+
+    @Override
+    public boolean test(Cca cca) {
+        requireNonNull(cca);
+        return persons.stream()
+                .anyMatch(person -> cca.containsEnrolledPerson(person));
+    }
+}
