232c6880cf976a4b5e4ba9a7785b4b5a0ba57bd0
Jivesh Mohan <jivesh@u.nus.edu>
2021-11-02 16:06:58 +0800

    Fix checkstyle issues

--- src/main/java/seedu/address/logic/commands/person/PersonFindCommand.java
+++ src/main/java/seedu/address/logic/commands/person/PersonFindCommand.java
@@ -2,9 +2,7 @@ package seedu.address.logic.commands.person;
 
 import static java.util.Objects.requireNonNull;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import seedu.address.commons.core.Messages;
